4395693b4e4d84c4f46309f3016c0ea9bca348ff
Mohnish Lokhande <mohnish.lokhande@brevo.com>
2024-08-07 11:18:59 +0530

    supress app

--- src/App.jsx
+++ src/App.jsx
@@ -49,13 +49,13 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" id="react-link">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} id="increase-count">
           count is {count}
         </button>
         <p>
